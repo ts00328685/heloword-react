ff79f1b6b0d3e2df2370ffe7761803116f29e5d2
Ryan Tzeng <ryan.tzeng@compax.at>
2022-03-18 14:38:04 +0800

    rebase 2

--- src/components/DigitButton.js
+++ src/components/DigitButton.js
@@ -1,5 +1,5 @@
 import { ACTIONS } from "../App"
 
 export default function DigitButton({ dispatch, digit }) {
-    return <button onClick={() => dispatch({ type: ACTIONS.DIGIT, payload: { digit   } })}>{digit}</button>
+    return <button onClick={() => dispatch({ type : ACTIONS.DIGIT, payload: { digit   } })}>{digit}</button>
 }
